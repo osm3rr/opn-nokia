ff92d47482d6869b3938b4348528989e082b5d81
osm3rr <osm3rr@gmail.com>
2022-05-19 15:25:53 -0400

    update script-opn

--- script_opn.py
+++ script_opn.py
@@ -40,8 +40,8 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_faults_lte_1 = df_faults_lte_1.drop( labels=0, axis=0 )
             # validation print
-            print( f'shape df_faults_lte_1: {df_faults_lte_1.shape}, file: {faults_lte_count}' )
-            #print( f'Fault LTE file: {faults_lte_count}' )
+            # print( f'shape df_faults_lte_1: {df_faults_lte_1.shape}, file: {faults_lte_count}' )
+            
         else:
             df_faults_lte_2 = pd.read_excel( io=xlsx_files_list[ xlsx_file ] )
             
@@ -49,13 +49,14 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_faults_lte_2 = df_faults_lte_2.drop( labels=0, axis=0 )
             
-            df_lte_total = pd.concat( [ df_faults_lte_1, df_faults_lte_2 ], ignore_index=True )
+            df_faults_lte_total = pd.concat( [ df_faults_lte_1, df_faults_lte_2 ],
+                                            ignore_index=True )
             
-            df_faults_lte_1 = df_lte_total.copy()
+            df_faults_lte_1 = df_faults_lte_total.copy()
             # validation print    
             #print( f'shape df_faults_lte_total: {df_lte_total.shape}' )
-            print( f'shape df_faults_lte_total: {df_lte_total.shape}, file: {faults_lte_count}' )
-            #print( f'Fault LTE file: {faults_lte_count}' )
+            # print( f'shape df_faults_lte_total: {df_lte_total.shape}, file: {faults_lte_count}' )
+            
         
         
     ## search GSM monitoring***
@@ -70,8 +71,8 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_gsm_1 = df_gsm_1.drop( labels=0, axis=0 )
             # validation print
-            print( f'shape df_gsm_1: {df_gsm_1.shape}, file: {gsm_monitoring_count}' )
-            #print( f'GSM files: {gsm_monitoring_count}' )
+            # print( f'shape df_gsm_1: {df_gsm_1.shape}, file: {gsm_monitoring_count}' )
+            
             
         else:
             df_gsm_2 = pd.read_excel( io=xlsx_files_list[ xlsx_file ] )
@@ -82,8 +83,8 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_gsm_1 = df_gsm_total.copy()
             # validation print    
-            print( f'shape df_gsm_total: {df_gsm_total.shape}, file: {gsm_monitoring_count}' )
-            #print( f'GSM file: {gsm_monitoring_count}' )
+            # print( f'shape df_gsm_total: {df_gsm_total.shape}, file: {gsm_monitoring_count}' )
+            
                 
             
     ## search LTE monitoring 
@@ -98,8 +99,8 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_lte_1 = df_lte_1.drop( labels=0, axis=0 )
             # validation print
-            print( f'shape df_lte_1: {df_lte_1.shape}, file: {lte_monitoring_count}' )
-            #print( f'LTE file: {lte_monitoring_count}' )
+            # print( f'shape df_lte_1: {df_lte_1.shape}, file: {lte_monitoring_count}' )
+            
             
         else:
             df_lte_2 = pd.read_excel( io=xlsx_files_list[ xlsx_file ] )
@@ -112,8 +113,8 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_lte_1 = df_lte_total.copy()
             # validation print    
-            print( f'shape df_lte_total: {df_lte_total.shape}, file: {lte_monitoring_count}' )
-            #print( f'LTE file: {lte_monitoring_count}' )
+            # print( f'shape df_lte_total: {df_lte_total.shape}, file: {lte_monitoring_count}' )
+            
         
                 
     ## search WBTS monitoring
@@ -128,8 +129,8 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_wbts_1 = df_wbts_1.drop( labels=0, axis=0 )
             # validation print
-            print( f'shape df_wbts_1: {df_wbts_1.shape}, file: {wbts_monitoring_count}' )
-            #print( f'WBTS file: {wbts_monitoring_count}' )
+            #print( f'shape df_wbts_1: {df_wbts_1.shape}, file: {wbts_monitoring_count}' )
+            
             
         else:
             df_wbts_2 = pd.read_excel( io=xlsx_files_list[ xlsx_file ] )
@@ -142,8 +143,8 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_wbts_1 = df_wbts_total.copy()
             # validation print    
-            print( f'shape df_wbts_total: {df_wbts_total.shape}, file: {wbts_monitoring_count}' )
-            #print( f'WBTS file: {wbts_monitoring_count}' )
+            # print( f'shape df_wbts_total: {df_wbts_total.shape}, file: {wbts_monitoring_count}' )
+            
 
         
     ## search WCDA monitoring
@@ -158,8 +159,8 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_wcdma_1 = df_wcdma_1.drop( labels=0, axis=0 )
             # validation print
-            print( f'shape df_wcdma_1: {df_wcdma_1.shape}, file: {wcdma_monitoring_count}' )
-            #print( f'WBTS file: {wbts_monitoring_count}' )
+            #print( f'shape df_wcdma_1: {df_wcdma_1.shape}, file: {wcdma_monitoring_count}' )
+            
             
         else:
             df_wcdma_2 = pd.read_excel( io=xlsx_files_list[ xlsx_file ] )
@@ -172,11 +173,68 @@ for xlsx_file in range( len( xlsx_files_list ) ):
             
             df_wcdma_1 = df_wcdma_total.copy()
             # validation print    
-            print( f'shape df_wcdma_total: {df_wcdma_total.shape}, file: {wcdma_monitoring_count}' )
-            #print( f'WBTS file: {wbts_monitoring_count}' )
+            #print( f'shape df_wcdma_total: {df_wcdma_total.shape}, file: {wcdma_monitoring_count}' )
+
 
     
     ## No valid files
     else:
         #print( "No valid files" )
         pass
+    
+# Remove duplicate values
+
+# Atributos para eliminar valores duplicados
+faults_lte_duplicate = [ 'Period start time', 'LNBTS name', 'ATT_INTER_ENB_HO (M8014C6)' ]
+gsm_monitoring_duplicate = [ 'Period start time', 'BSC name', 'BTS name' ]
+lte_monitoring_duplicate = [ 'Period start time', 'LNCEL name' ]
+wbts_monitoring_duplicate = [ 'Period start time', 'WBTS name', 'WBTS ID' ]
+wcdma_monitoring_duplicate = [ 'Period start time', 'WCEL name', 'WCEL ID' ]
+
+# Removiendo valores duplicados
+
+print( f'shape before delete duplicate FAULT LTE: {df_faults_lte_total.shape}' )
+
+# Eliminar duplicados en faults lte
+df_faults_lte_filtered = df_faults_lte_total.drop_duplicates( 
+                                                             subset=faults_lte_duplicate,
+                                                             ignore_index=True 
+                                                             )
+
+print( f'shape after delete duplicate FAULT LTE: {df_faults_lte_filtered.shape}' )
+
+# Eliminar duplicados en gsm total
+print( f'shape before delete duplicate GSM: {df_gsm_total.shape}' )
+df_gsm_filtered = df_gsm_total.drop_duplicates( 
+                                               subset=gsm_monitoring_duplicate,
+                                                             ignore_index=True 
+                                                             )
+
+print( f'shape after delete duplicate GSM: {df_gsm_filtered.shape}' )
+
+# Eliminar duplicados en LTE total
+print( f'shape before delete duplicate LTE: {df_lte_total.shape}' )
+df_lte_filtered = df_lte_total.drop_duplicates( 
+                                               subset=lte_monitoring_duplicate,
+                                                        ignore_index=True 
+                                                              )
+
+print( f'shape after delete duplicate LTE: {df_lte_filtered.shape}' )
+
+# Eliminar duplicados en WBTS total [pendind]
+print( f'shape before delete duplicate WBTS: {df_wbts_total.shape}' )
+df_wbts_filtered = df_wbts_total.drop_duplicates( 
+                                               subset=wbts_monitoring_duplicate,
+                                                        ignore_index=True 
+                                                              )
+
+print( f'shape after delete duplicate WBTS: {df_wbts_filtered.shape}' )
+
+# Eliminar duplicados en WCDMA total [pendind]
+print( f'shape before delete duplicate WCDMA: {df_wcdma_total.shape}' )
+df_wcdma_filtered = df_wcdma_total.drop_duplicates( 
+                                               subset=wcdma_monitoring_duplicate,
+                                                        ignore_index=True 
+                                                              )
+
+print( f'shape after delete duplicate WCDMA: {df_wcdma_filtered.shape}' )
