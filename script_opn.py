412846597d1b84cf5776d7d0ab261410193b9ad9
osm3rr <osm3rr@gmail.com>
2022-06-12 16:28:44 -0400

    update script_opn

--- script_opn.py
+++ script_opn.py
@@ -1,11 +1,12 @@
 # necessary libraries
 import pandas as pd
+import numpy as np
 import glob
 from pathlib import Path
 
 
 # base path of files location
-path = "/home/opm/Documentos-opm/OPN_COLOMBIA/ANALISYS/input_files"
+path = "/home/opm/Documentos-opm/OPN_COLOMBIA/ANALISYS/input"
 
 # List with the name of all excel files
 xlsx_files_list = glob.glob( path + "/*.xlsx" )
@@ -188,8 +189,188 @@ for xlsx_file in range( len( xlsx_files_list ) ):
         #print( "No valid files" )
         pass
 
+#***************************************************
+# check the attributes for early warning reporting
+#**************************************************
+
+# check the attributes for early warning reporting 4G
+print("******************************************")
+print( "check the attributes for early warning reporting 4G (FAULTS LTE)..." )
+print("******************************************")
+
+# attributes available in lte faults
+attribte_list_faults_lte = df_faults_lte_total.columns.to_list()
+
+# existence of the "LNBTS type" attribute
+if "LNBTS type" not in attribte_list_faults_lte:
+    
+    # determine total rows
+    rows_faults_lte = df_faults_lte_total.shape
+    
+    # list with total NAN values
+    lnbts_type_faults_lte = [ np.NaN for value in range( rows_faults_lte[0] ) ]
+    
+    # insert in the df the number of empty values
+    df_faults_lte_total.insert(
+                                loc = 2, 
+                                column = "LNBTS type", 
+                                value = lnbts_type_faults_lte
+                               )
+
+# if the file exists, no action is taken
+else:
+    pass
+
+# Change of name of the attributes according to the template
+df_faults_lte_total = df_faults_lte_total.rename( columns = 
+                                                    { 
+                                                    "avgRTT_15Min" : "avgRTT_15Min (M51132C0)", 
+                                                    "maxRTT_15Min" : "maxRTT_15Min (M51132C1)"
+                                                    }
+                                                )
+
+print( "***************************************************" )
+print( "4G parameter correspondence (FAULTS LTE): ok!!! " )
+print("****************************************************")
+print('\n')
+
+# **fix attribute names read in lte**
+print("*****************************************************************")
+print( "check the attributes for early warning reporting 4G (LTE)..." )
+print("*****************************************************************")
+
+# control flag for tests in this notebook
+rename_count_lte = 0
+
+# attributes available in lte
+attribte_list_lte = df_lte_total.columns.to_list()
+
+# existence of the "LNBTS type" attribute
+if "LNBTS type" not in attribte_list_lte:
+    
+    # determine total rows
+    rows_lte = df_lte_total.shape
+    
+    # list with total NAN values
+    lnbts_type_lte = [ np.NaN for value in range( rows_lte[0] ) ]
+    
+    # insert in the df the number of empty values
+    df_lte_total.insert(
+                                loc = 2, 
+                                column = "LNBTS type", 
+                                value = lnbts_type_lte
+                               )
+# if the file exists, no action is taken
+else:
+    pass
+
+# Change of name of the attributes according to the template
+
+if rename_count_lte < 1:
+    
+    rename_count_lte += 1
+    
+    df_lte_total = df_lte_total.rename( columns = { 
+                                                "AVG_RTWP_RX_ANT_1 (M8005C306)" : "AVG_RTWP_RX_ANT_1", 
+                                                "AVG_RTWP_RX_ANT_2 (M8005C307)" : "AVG_RTWP_RX_ANT_2", 
+                                                "AVG_RTWP_RX_ANT_3 (M8005C308)" : "AVG_RTWP_RX_ANT_3", 
+                                                "AVG_RTWP_RX_ANT_4 (M8005C309)" : "AVG_RTWP_RX_ANT_4"
+                                                }
+                                    )
+else:
+    pass
+
+# the list of column names is updated
+attribte_list_lte = df_lte_total.columns.to_list()
+
+
+# Check the existence of the attributes, if they do not exist they are added
+if 'AVG_RTWP_RX_ANT_1 (M8005C306)' not in attribte_list_lte:
+    
+   df_lte_total[ 'AVG_RTWP_RX_ANT_1 (M8005C306)' ] = df_lte_total[ 'AVG_RTWP_RX_ANT_1' ] / 10
+
+else:
+    pass
+
+if 'AVG_RTWP_RX_ANT_2 (M8005C307)' not in attribte_list_lte:
+    
+    df_lte_total[ 'AVG_RTWP_RX_ANT_2 (M8005C307)' ] = df_lte_total[ 'AVG_RTWP_RX_ANT_2' ] / 10
+    
+else:
+    pass
+
+
+if 'AVG_RTWP_RX_ANT_3 (M8005C308)' not in attribte_list_lte:
+    
+    df_lte_total[ 'AVG_RTWP_RX_ANT_3 (M8005C308)' ] = df_lte_total[ 'AVG_RTWP_RX_ANT_3' ] / 10
+    
+else:
+    pass
+
+if 'AVG_RTWP_RX_ANT_4 (M8005C309)' not in attribte_list_lte:
+    
+    df_lte_total[ 'AVG_RTWP_RX_ANT_4 (M8005C309)' ] = df_lte_total[ 'AVG_RTWP_RX_ANT_4' ] / 10
+    
+else:
+    pass
+
+
+if 'Sector' not in attribte_list_lte:
+    # "Sector": Last two characters of the 'LNCEL name' attribute
+    df_lte_total[ 'Sector' ] = df_lte_total[ 'LNCEL name' ].apply( lambda x: x[ -2: ] )
+else:
+    pass
+
+# the list of column names is updated
+attribte_list_lte = df_lte_total.columns.to_list()
+
+print( "*******************************" )
+print( "4G parameter correspondence (LTE): ok!!! " )
+print("******************************************")
+print('\n')
+
+# **fix attribute names read in WBTS and WCDMA**
+print("*****************************************************************")
+print( "check the attributes for early warning reporting 3G (WBTS, WCDMA)..." )
+print("*****************************************************************")
+print('\n')
+
+
+print( "***************************************************" )
+print( "3G parameter correspondence (WBTS, WCDMA): ok!!! " )
+print("****************************************************")
+print('\n')
+
+print("*****************************************************************")
+print( "check the attributes for early warning reporting 2G (GSM)..." )
+print("*****************************************************************")
+
+# attributes available in gsm
+attribte_list_gsm = df_gsm_total.columns.to_list()
+
+# verify existence of the attribute "Zlinea"
+if "Zlinea" not in attribte_list_gsm:
+    
+    # determine total rows
+    rows_gsm = df_gsm_total.shape
+    
+    # list with total NAN values
+    zlinea_gsm = [ np.NaN for value in range( rows_gsm[0] ) ]
+    
+    # insert in the df the number of empty values
+    df_gsm_total[ "Zlinea" ] = zlinea_gsm
+    
+# if the file exists, no action is taken
+else:
+    pass
+
+print( "***************************************************" )
+print( "2G parameter correspondence (GSM): ok!!! " )
+print("****************************************************")
+print('\n')
+
 #************************************************
-# normaliza el nombre de los sitios por archivo 
+# normalize the name of the sites per file
 #************************************************
 
 
@@ -260,10 +441,10 @@ lte_monitoring_duplicate = [ 'Period start time', 'LNCEL name' ]
 wbts_monitoring_duplicate = [ 'Period start time', 'WBTS name', 'WBTS ID' ]
 wcdma_monitoring_duplicate = [ 'Period start time', 'WCEL name', 'WCEL ID' ]
 
-# Removing duplicate values FAULT LTE
+# Removing duplicate values LTE FAULT
 print( f'Shape before remove duplicates in LTE FAULT: {df_faults_lte_total.shape}' )
 
-# Eliminar duplicados en faults lte
+# Removing duplicate values LTE FAULT
 df_faults_lte_unique = df_faults_lte_total.drop_duplicates( 
                                                              subset=faults_lte_duplicate,
                                                              ignore_index=True 
@@ -271,21 +452,21 @@ df_faults_lte_unique = df_faults_lte_total.drop_duplicates(
 
 print( f'Shape after remove duplicates LTE FAULT: {df_faults_lte_unique.shape}' )
 
-# Eliminar duplicados en gsm total
+# remove duplicates in GSM total
 print( f'Shape before remove duplicates GSM: {df_gsm_total.shape}' )
 df_gsm_unique = df_gsm_total.drop_duplicates( 
-                                               subset=gsm_monitoring_duplicate,
-                                                             ignore_index=True 
-                                                             )
+                                            subset=gsm_monitoring_duplicate,
+                                            ignore_index=True 
+                                            )
 
 print( f'Shape after remove duplicates GSM: {df_gsm_unique.shape}' )
 
-# Eliminar duplicados en LTE total
+# Remove Duplicates in Full LTE
 print( f'Shape before remove duplicates LTE: {df_lte_total.shape}' )
 df_lte_unique = df_lte_total.drop_duplicates( 
-                                               subset=lte_monitoring_duplicate,
-                                                        ignore_index=True 
-                                                              )
+                                            subset=lte_monitoring_duplicate,
+                                            ignore_index=True 
+                                            )
 
 print( f'Shape after remove duplicates LTE: {df_lte_unique.shape}' )
 
@@ -293,17 +474,17 @@ print( f'Shape after remove duplicates LTE: {df_lte_unique.shape}' )
 print( f'Shape before remove duplicates WBTS: {df_wbts_total.shape}' )
 df_wbts_unique = df_wbts_total.drop_duplicates( 
                                                subset=wbts_monitoring_duplicate,
-                                                        ignore_index=True 
-                                                              )
+                                               ignore_index=True 
+                                            )
 
 print( f'Shape after remove duplicates WBTS: {df_wbts_unique.shape}' )
 
-# Eliminar duplicados en WCDMA total [pendind]
+# Remove Duplicates in WCDMA Total
 print( f'Shape before remove duplicates WCDMA: {df_wcdma_total.shape}' )
 df_wcdma_unique = df_wcdma_total.drop_duplicates( 
-                                               subset=wcdma_monitoring_duplicate,
-                                                        ignore_index=True 
-                                                              )
+                                                subset=wcdma_monitoring_duplicate,
+                                                ignore_index=True 
+                                                )
 
 print( f'Shape after remove duplicates WCDMA: {df_wcdma_unique.shape}' )
 
@@ -378,10 +559,12 @@ print("******************************************")
 print( "Exporting files filtered by site..." )
 print("******************************************")
 
+output_folder = 'output'
+
 # Exporting LTE FAULTS files
 print("Exporting LTE FAULTS files ... ")
 for key, value in d_f_lte.items():
-    file_path = Path( f'output_files/fallas_lte_{key}.csv' )
+    file_path = Path( f'{output_folder}/fallas_lte_{key}.csv' )
     file_path.parent.mkdir( parents=True, exist_ok=True )
     #print(f'{key}: {value.shape}')
     value.to_csv(file_path)
@@ -391,7 +574,7 @@ print("Exporting LTE FAULTS files OK!")
 # Exporting GSM files
 print("Exporting GSM files ... ")
 for key, value in d_gsm.items():
-    file_path = Path( f'output_files/gsm_{key}.csv' )
+    file_path = Path( f'{output_folder}/gsm_{key}.csv' )
     file_path.parent.mkdir( parents=True, exist_ok=True )
     #print(f'{key}: {value.shape}')
     value.to_csv(file_path)
@@ -401,7 +584,7 @@ print("Exporting GSM files OK!")
 # Exporting LTE files
 print("Exporting LTE files... ")
 for key, value in d_lte.items():
-    file_path = Path( f'output_files/lte_{key}.csv' )
+    file_path = Path( f'{output_folder}/lte_{key}.csv' )
     file_path.parent.mkdir( parents=True, exist_ok=True )
     #print(f'{key}: {value.shape}')
     value.to_csv(file_path)
@@ -412,7 +595,7 @@ print("Exporting LTE files OK")
 # Exporting WBTS files
 print("Exporting WBTS files... ")
 for key, value in d_wbts.items():
-    file_path = Path( f'output_files/wbts_{key}.csv' )
+    file_path = Path( f'{output_folder}/wbts_{key}.csv' )
     file_path.parent.mkdir( parents=True, exist_ok=True )
     #print(f'{key}: {value.shape}')
     value.to_csv(file_path)
@@ -422,10 +605,9 @@ print("Exporting WBTS files OK!")
 # Exporting WCDMA files
 print("Exporting WCDMA files... ")
 for key, value in d_wcdma.items():
-    file_path = Path( f'output_files/wcdma_{key}.csv' )
+    file_path = Path( f'{output_folder}/wcdma_{key}.csv' )
     file_path.parent.mkdir( parents=True, exist_ok=True )
     #print(f'{key}: {value.shape}')
     value.to_csv(file_path)
     
 print("Exporting WCDMA files OK!")
-
